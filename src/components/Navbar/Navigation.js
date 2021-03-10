8b25165a7552721d7a5d540f3066f51a0e001e91
tanvirul15 <tanvirul15-6117@diu.edu.bd>
2021-03-10 16:43:33 +0600

    Basic setup

---
+++ src/components/Navbar/Navigation.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Form, FormControl, Nav, Navbar } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+
+const Navigation = () => {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#features">Features</Nav.Link>
+        <Nav.Link href="#pricing">Pricing</Nav.Link>
+      </Nav>
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="outline-info">Search</Button>
+      </Form>
+    </Navbar>
+  );
+};
+
+export default Navigation;
